cb0b20f6aa553ae05497cca0b5fc442412da754e
Serters <theweeb4life@gmail.com>
2023-10-24 10:01:17 +0200

    Path update

--- GUI.py
+++ GUI.py
@@ -16,7 +16,7 @@ main_frame.columnconfigure(0, weight=1)
 logo_label = tk.Label(main_frame, text="VTSFIT", font=("Helvetica", 24), background=colors.c1, foreground=colors.c2)
 logo_label.grid(row=0, column=0, padx=10, pady=10)
 
-folder_path = 'C:/Users/Korisnik/Desktop/Projekti/Py PROJEKAT/videoSnimci'
+folder_path = "VTS_Apps_Team_AI/videoSnimci"
 options_list = utils.get_video_file_names(folder_path)
 selected_option = tk.StringVar(main_frame) 
 selected_option.set("Izaberite video")
@@ -50,9 +50,6 @@ def get_video():
     video = 'videoSnimci/' + selected_option.get()
     print(video)
     return str(video)
-
-submit_button = tk.Button(root, text='Submit', command=get_video()) 
-submit_button.pack() 
 
 # Button commands
 def command_one():
