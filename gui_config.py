d7e0294cd7e52ddd004373dc6e350b72c14f2fa8
Serters <theweeb4life@gmail.com>
2023-11-07 22:32:49 +0100

    clean code GUI

--- gui_config.py
+++ gui_config.py
@@ -1,12 +1,8 @@
-class ColorPalette:
-    background = '#1c1c22'
-    object = '#2a2a34'
-    accent = '#0e74bb'
-    text = 'WHITE'
-
-class Size:
-    padding = 10
-    font = 25
+class Colors:
+    background = "#1c1c22"
+    object = "#2a2a34"
+    accent = "#0e74bb"
+    text = "WHITE"
 
 class Spacer:
     source = 100
@@ -14,15 +10,47 @@ class Spacer:
     exercise = 40
     display = 20
 
-#TODO
 class Text:
+    font = "Arial"
     size = 25
-    font = 'Arial'
-    format = 'bold'
+    format = "bold"
+    text = [font, size, format]
 
-#TODO
 class Padding:
     source = 0
     source_buttons = 0
-    exercise = 0
+    exercise = 10
     display = 0
+
+class Styles:
+    source_button = {
+        "font": Text.text,
+        "background": Colors.object,
+        "foreground": Colors.text,
+        "activebackground": Colors.object,
+        "activeforeground": Colors.accent,
+        "highlightthickness": 2,
+        "highlightbackground": Colors.accent,
+        "highlightcolor": "WHITE",
+        "width": 23,
+        "height": 2,
+        "border": 0,
+    }
+    button_frame = {
+        "highlightbackground": Colors.accent,
+        "highlightthickness": 5,
+        "background": Colors.accent,
+        "border": 0,
+    }
+    optionmenu = {
+        "font": Text.text,
+        "background": Colors.object,
+        "foreground": Colors.text,
+        "activebackground": Colors.object,
+        "activeforeground": Colors.accent,
+        "highlightthickness": 5,
+        "highlightbackground": Colors.accent,
+        "highlightcolor": "WHITE",
+        "width": 24,
+        "border": 0,
+    }
--- gui_functions.py
+++ gui_functions.py
@@ -1,7 +1,8 @@
-from gui_config import ColorPalette as c
-from gui_config import Size as s
+from gui_config import Colors as c
+from gui_config import Padding as p
 from gui_config import Spacer as sp
 from gui_config import Text as t
+from gui_config import Styles as st
 import utils
 import tkinter as tk
 import cv2
@@ -9,6 +10,17 @@ import cv2
 selected_video = ''
 selected_exercise = 0
 frame_counter = 0
+
+def create_logo(frame, spacer_pady, columnspan):
+    logo_label = tk.Label(frame, text="VTŠFIT", font=("Helvetica", 24, "bold"), background=c.background, foreground=c.accent)
+    logo_label.grid(row=0, column=0, padx=10, pady=10, columnspan=columnspan)
+
+    image = tk.PhotoImage(file="gui_images/gui_line.png")
+    image_label = tk.Label(frame, image=image, borderwidth=0, relief="flat")
+    image_label.grid(row=1, column=0, columnspan=columnspan)
+    
+    spacer = tk.Label(frame, pady=spacer_pady, bg=c.background)
+    spacer.grid(row=2, column=0, columnspan=columnspan)
 
 #Soruce
 def select_existing_video(selected_option, root, source_frame):
@@ -23,38 +35,19 @@ def record_new_video():
     pass 
 
 def create_source_frame(root):
-
     source_frame = tk.Frame(root, bg=c.background)
     source_frame.pack()
     source_frame.pack(fill=tk.BOTH, expand=True)
-
-    logo_label = tk.Label(source_frame, text="VTŠFIT", font=("Helvetica", 24, "bold"), background=c.background, foreground=c.accent)
-    logo_label.grid(row=0, column=0, padx=10, pady=10)
-    image = tk.PhotoImage(file="gui_images/gui_line.png")
-    image_label = tk.Label(source_frame, image=image, borderwidth=0, relief="flat")
-    image_label.grid(row=1, column=0)
-    spacer = tk.Label(source_frame, pady=sp.source, bg=c.background)
-    spacer.grid(row=2, column=0)
+    create_logo(source_frame, sp.source, 1)
 
     options_list = utils.get_video_file_names()
     selected_option = tk.StringVar(source_frame)
     selected_option.set("Izaberite video")
     option_menu = tk.OptionMenu(source_frame, selected_option, *options_list)
-    optionmenu_style = {
-        "font": ("Arial", s.font, "bold"),
-        "background": c.object,
-        "foreground": c.text,
-        "activebackground": c.object,
-        "activeforeground": c.accent,
-        "highlightthickness": 5,
-        "highlightbackground": c.accent,
-        "highlightcolor": "WHITE",
-        "width": 24,
-        "border": 0,
-    }
-    option_menu.configure(**optionmenu_style)
+
+    option_menu.configure(**st.optionmenu)
     option_menu["menu"].configure(
-        font=("Arial", s.font, "bold"),
+        font=t.text,
         background=c.object,
         foreground=c.text,
         activebackground=c.object,
@@ -66,91 +59,31 @@ def create_source_frame(root):
     spacer_two = tk.Label(source_frame, pady=sp.source_buttons, bg=c.background)
     spacer_two.grid(row=4, column=0)
 
-    button_one_border = tk.Frame(
-                        source_frame, 
-                        highlightbackground = c.accent,
-                        highlightthickness = 5,
-                        background=c.accent,
-                        border=0
-                        )
-    button_one = tk.Button(                       
-                                button_one_border,
-                                font = ('Arial', s.font, 'bold'),
-                                background=c.object,
-                                foreground= c.text,
-                                activebackground= c.object,
-                                activeforeground= c.accent,
-                                highlightthickness= 2,
-                                highlightbackground= c.accent,
-                                highlightcolor="WHITE",
-                                width=23,
-                                height=2, 
-                                border=0,
-                                text='Izaberi postojeći video',
-                                command=lambda: select_existing_video(selected_option, root, source_frame)                               
-                                )
+    button_one_border = tk.Frame(source_frame, st.button_frame)
+    button_one = tk.Button(button_one_border, text='Izaberi postojeći video', **st.source_button,
+                        command=lambda: select_existing_video(selected_option, root, source_frame))
     button_one_border.grid(row=5, column=0, columnspan=1)
     button_one.grid(column=0, row=0)
 
     spacer_three = tk.Label(source_frame, pady=sp.source_buttons, bg=c.background)
     spacer_three.grid(row=6, column=0)
 
-    button_two_border = tk.Frame(
-                        source_frame, 
-                        highlightbackground = c.accent,
-                        highlightthickness = 5,
-                        background=c.accent,
-                        border=0
-                        )
-    button_two = tk.Button(                       
-                                button_two_border,
-                                font = ('Arial', s.font, 'bold'),
-                                background=c.object,
-                                foreground= c.text,
-                                activebackground= c.object,
-                                activeforeground= c.accent,
-                                highlightthickness= 2,
-                                highlightbackground= c.accent,
-                                highlightcolor="WHITE",
-                                width=23,
-                                height=2, 
-                                border=0,
-                                text='Vežbaj uživo',
-                                command=workout_live()   
-    )
+    button_two_border = tk.Frame(source_frame, st.button_frame)
+    button_two = tk.Button(button_two_border, text='Vežbaj uživo', **st.source_button,
+                        command=lambda: workout_live())
     button_two_border.grid(row=7, column=0, columnspan=1)
     button_two.grid(column=0, row=0)
 
     spacer_four = tk.Label(source_frame, pady=sp.source_buttons, bg=c.background)
     spacer_four.grid(row=8, column=0)
 
-    button_three_border = tk.Frame(
-                        source_frame, 
-                        highlightbackground = c.accent,
-                        highlightthickness = 5,
-                        background=c.accent,
-                        border=0
-                        )
-    button_three = tk.Button(                       
-                                button_three_border,
-                                font = ('Arial', s.font, 'bold'),
-                                background=c.object,
-                                foreground= c.text,
-                                activebackground= c.object,
-                                activeforeground= c.accent,
-                                highlightthickness= 2,
-                                highlightbackground= c.accent,
-                                highlightcolor="WHITE",
-                                width=23,
-                                height=2, 
-                                border=0,
-                                text='Snimi novi video',
-                                command=record_new_video()   
-                                )
+    button_three_border = tk.Frame(source_frame, st.button_frame)
+    button_three = tk.Button(button_three_border, text='Snimi novi video', **st.source_button,
+                            command=lambda: record_new_video())
     button_three_border.grid(row=9, column=0, columnspan=1)
     button_three.grid(row=0, column=0)
 
-#EXERCISE
+#Exercise
 def select_exercise(exercise, root, exercise_frame):
     global selected_exercise
     selected_exercise = exercise
@@ -162,57 +95,23 @@ def create_exercise_frame(root, source_frame):
     exercise_frame = tk.Frame(root, bg=c.background)
     exercise_frame.pack()
     exercise_frame.pack(fill=tk.BOTH, expand=True)
-
-    logo_label = tk.Label(exercise_frame, text="VTŠFIT", font=("Helvetica", 24, "bold"), background=c.background, foreground=c.accent)
-    logo_label.grid(row=0, column=0, padx=10, pady=10, columnspan=5)
-    image = tk.PhotoImage(file="gui_images/gui_line.png")
-    image_label = tk.Label(exercise_frame, image=image, borderwidth=0, relief="flat")
-    image_label.grid(row=1, column=0, columnspan=5)
-    spacer = tk.Label(exercise_frame, pady=sp.exercise, bg=c.background)
-    spacer.grid(row=2, column=0, columnspan=5)
+    create_logo(exercise_frame, sp.exercise, 5)
 
     exercises_frame = tk.Frame(exercise_frame, background=c.background)
     exercises_frame.grid(row=3, column=1, columnspan=3)
+    exercise_images = []
+    for i in range(1, 7):
+        image_path = f"gui_images/gui_exercises/gui_exercise_{i}.png"
+        exercise_image = tk.PhotoImage(file=image_path)
+        exercise_images.append(exercise_image)
 
-    exercise_1 = tk.PhotoImage(file="gui_images/gui_exercises/gui_exercise_1.png")
-    exercise_2 = tk.PhotoImage(file="gui_images/gui_exercises/gui_exercise_2.png")
-    
-    exercise_3 = tk.PhotoImage(file="gui_images/gui_exercises/gui_exercise_3.png")
-    exercise_4 = tk.PhotoImage(file="gui_images/gui_exercises/gui_exercise_4.png")
-    
-    exercise_5 = tk.PhotoImage(file="gui_images/gui_exercises/gui_exercise_5.png")
-    exercise_6 = tk.PhotoImage(file="gui_images/gui_exercises/gui_exercise_6.png")
-
-    exercise_label_1 = tk.Label(exercises_frame, image=exercise_1, border=0, relief='flat')
-    exercise_label_1.image = exercise_1
-    exercise_label_1.grid(row=0, column=0, padx=s.padding, pady=s.padding)
-    exercise_label_1.bind("<Button-1>", lambda event: select_exercise(0, root, exercise_frame))
-
-    exercise_label_2 = tk.Label(exercises_frame, image=exercise_2, border=0, relief='flat')
-    exercise_label_2.image = exercise_2
-    exercise_label_2.grid(row=1, column=0, padx=s.padding, pady=s.padding)
-    exercise_label_2.bind("<Button-1>", lambda event: select_exercise(1, root, exercise_frame))    
-
-    exercise_label_3 = tk.Label(exercises_frame, image=exercise_3, border=0, relief='flat')
-    exercise_label_3.image = exercise_3
-    exercise_label_3.grid(row=0, column=1, padx=s.padding, pady=s.padding)
-    exercise_label_3.bind("<Button-1>", lambda event: select_exercise(2, root, exercise_frame))
-
-    exercise_label_4 = tk.Label(exercises_frame, image=exercise_4, border=0, relief='flat')
-    exercise_label_4.image = exercise_4
-    exercise_label_4.grid(row=1, column=1, padx=s.padding, pady=s.padding)    
-    exercise_label_4.bind("<Button-1>", lambda event: select_exercise(3, root, exercise_frame))
-
-    exercise_label_5 = tk.Label(exercises_frame, image=exercise_5, border=0, relief='flat')
-    exercise_label_5.image = exercise_5
-    exercise_label_5.grid(row=0, column=2, padx=s.padding, pady=s.padding)
-    exercise_label_5.bind("<Button-1>", lambda event: select_exercise(4, root, exercise_frame))
-
-    exercise_label_6 = tk.Label(exercises_frame, image=exercise_6, border=0, relief='flat')
-    exercise_label_6.image = exercise_6
-    exercise_label_6.grid(row=1, column=2, padx=s.padding, pady=s.padding)
-    exercise_label_6.bind("<Button-1>", lambda event: select_exercise(5, root, exercise_frame))
-
+    for i, exercise_image in enumerate(exercise_images):
+        row = i // 3
+        col = i % 3
+        exercise_label = tk.Label(exercises_frame, image=exercise_image, border=0, relief='flat')
+        exercise_label.image = exercise_image
+        exercise_label.grid(row=row, column=col, padx=p.exercise, pady=p.exercise)
+        exercise_label.bind("<Button-1>", lambda event, i=i: select_exercise(i, root, exercise_frame))   
 
 #Display
 def create_display_frame(root, exercise_frame):
@@ -221,14 +120,7 @@ def create_display_frame(root, exercise_frame):
     display_frame = tk.Frame(root, bg=c.background)
     display_frame.pack()
     display_frame.pack(fill=tk.BOTH, expand=True)
-
-    logo_label = tk.Label(display_frame, text="VTŠFIT", font=("Helvetica", 24, "bold"), background=c.background, foreground=c.accent)
-    logo_label.grid(row=0, column=0, padx=10, pady=10, columnspan=3)
-    image = tk.PhotoImage(file="gui_images/gui_line.png")
-    line_label = tk.Label(display_frame, image=image, borderwidth=0, relief="flat")
-    line_label.grid(row=1, column=0, columnspan=3)
-    spacer = tk.Label(display_frame, pady=sp.display, bg=c.background)
-    spacer.grid(row=2, column=0, columnspan=3)
+    create_logo(display_frame, sp.display, 3)
 
     vertical_frame = tk.Frame(display_frame, background=c.background)
     vertical_frame.grid(row=3, column=1)
@@ -240,15 +132,12 @@ def create_display_frame(root, exercise_frame):
 
     def update_display():
         global frame_counter
-        global selected_video
-        global selected_exercise
 
         cap = cv2.VideoCapture(selected_video)
         cap.set(cv2.CAP_PROP_POS_FRAMES, frame_counter)
         ret, frame = cap.read()
         if ret:
             processed_frame = utils.process_frame(frame, selected_exercise)
-
             utils.resize_frame(processed_frame)
             cap.release()
 
@@ -257,38 +146,33 @@ def create_display_frame(root, exercise_frame):
             display_label.image = image
             display_label.grid(row=0, column=0)
 
-            frame_counter = frame_counter + 1
+            frame_counter += 1
             root.after(1, update_display)
         else:
              print('End of the video')
              frame_counter = 0
              cap.release()
 
-    button_play_border = tk.Frame(
-                        display_frame, 
-                        highlightbackground = c.accent,
-                        highlightthickness = 5,
-                        background=c.accent,
-                        border=0
-                        )
-    button_play = tk.Button(                       
-                                button_play_border,
-                                font = ('Arial', s.font, 'bold'),
-                                background=c.object,
-                                foreground= c.text,
-                                activebackground= c.object,
-                                activeforeground= c.accent,
-                                highlightthickness= 2,
-                                highlightbackground= c.accent,
-                                highlightcolor="WHITE",
-                                width=21,
-                                height=1, 
-                                border=0,
-                                text='Play',
-                                command=update_display                               
-                                )
-    button_play_border.grid(row=4,column=1, pady=10)
-    button_play.grid(column=0, row=0)
+    play_button_border = tk.Frame(display_frame, st.button_frame)
+    play_button = tk.Button(                       
+                            play_button_border,
+                            font = t.text,
+                            background=c.object,
+                            foreground= c.text,
+                            activebackground= c.object,
+                            activeforeground= c.accent,
+                            highlightthickness= 2,
+                            highlightbackground= c.accent,
+                            highlightcolor="WHITE",
+                            width=21,
+                            height=1, 
+                            border=0,
+                            text='Play',
+                            command=update_display                               
+                            )
+    play_button_border.grid(row=4,column=1, pady=10)
+    play_button.grid(column=0, row=0)
 
+#Other
 def close_window(root, event):
     root.destroy()
